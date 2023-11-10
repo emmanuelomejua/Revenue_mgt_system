0a642e8cbaccb806600b20d64afbed69046e2e6e
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-11-11 00:40:33 +0100

    tjahh

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import './login.scss'
 
 const Login = () => {
   return (
-    <div>
-      Login
+    <div className='login'>
+      <form className="login__cont">
+        <img src="ifadore.png" alt="" />
+        <h2>Login</h2>
+        <input type="email" placeholder='Email' required min={8}/>
+        <input type="password" placeholder='Password' required min={8} />
+        <button type='submit'>Login</button>
+      </form>
     </div>
   )
 }

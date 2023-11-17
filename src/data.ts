db2285c3cc6d5a30c09e1abde8de95693ab789e7
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-11-17 20:14:46 +0100

    M

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,24 @@
-
-
-export const items = [
-
-]
+export const rows = [
+    { id: 1, title: 'Snow', amount: 2300, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+    { id: 2, title: 'Lannister', amount: 2300, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+    { id: 3, title: 'Lannister', amount: 2300, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+    { id: 4, title: 'Stark', amount: 2300, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+    { id: 5, title: 'Targaryen', amount: 2300, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+    { id: 6, title: 'Melisandre', amount: 2500, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+    { id: 7, title: 'Clifford', amount: 12990, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+    { id: 8, title: 'Frances', amount: 1200, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+    { id: 10, title: 'Roxie', amount: 1200, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+    { id: 11, title: 'Roxie', amount: 1200, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+    { id: 12, title: 'Roxie', amount: 1200, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+    { id: 13, title: 'Roxie', amount: 1300, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+    { id: 14, title: 'Roxie', amount: 1300, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji'},
+    { id: 15, title: 'Roxie', amount: 1300, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+    { id: 16, title: 'Roxie', amount: 1300, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+    { id: 17, title: 'Roxie', amount: 1300, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+    { id: 18, title: 'Roxie', amount: 1300, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+    { id: 19, title: 'Roxie', amount: 1300, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+    { id: 20, title: 'Roxie', amount: 1300, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+    { id: 21, title: 'Roxie', amount: 1300, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+    { id: 22, title: 'Roxie', amount: 1300, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+    { id: 23, title: 'Roxie', amount: 1300, address: '2 Millenium Road', phone: '090117585939', name: 'Ayodeji' },
+  ];

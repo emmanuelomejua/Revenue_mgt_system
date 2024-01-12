a17b5ab7e6364cac5943a95ff682cdd5e1306394
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2024-01-12 10:29:27 +0100

    U

---
+++ src/services/context/polltaxContext/polltaxActions.ts
@@ -0,0 +1,22 @@
+export const getPollTaxStart = () => ({
+    type: 'GET_POLLTAX_START'
+})
+
+export const getPollTaxSuccess = () => ({
+    type: 'GET_POLLTAX_SUCCESS'
+})
+
+export const getPollTaxFail = () => ({
+    type: 'GET_POLLTAX_FAIL'
+})
+
+//create
+export const createPollTaxStart = () => ({
+    type: 'GET_POLLTAX_START'
+})
+export const createPollTaxSuccess = () => ({
+    type: 'GET_POLLTAX_SUCESS'
+})
+export const createPollTaxFail = () => ({
+    type: 'GET_POLLTAX_FAIL'
+})
